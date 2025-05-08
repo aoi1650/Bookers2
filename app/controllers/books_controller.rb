359a293a1c67532d1397adf2a1e539fcb935d1d9
EC2 Default User <ec2-user@ip-172-31-45-159.ap-northeast-1.compute.internal>
2025-05-08 04:20:22 +0000

    [Add] bookのindexページの左側にユーザー情報を表示できるようにuserのmodel, bookのcontrollerに追記

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,5 +3,20 @@ class BooksController < ApplicationController
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
